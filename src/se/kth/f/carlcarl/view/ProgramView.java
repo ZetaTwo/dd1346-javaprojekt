8757e6ce662b04469f774788003306a2ec042251
Calle Svensson <calle.svensson@zeta-two.com>
2013-03-06 16:36:15 +0100

    Close all chats on exit.

--- src/se/kth/f/carlcarl/view/ProgramView.java
+++ src/se/kth/f/carlcarl/view/ProgramView.java
@@ -19,6 +19,8 @@ import se.kth.f.carlcarl.model.ProgramSettingsMdl;
 import java.awt.Color;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.File;
 import java.io.IOException;
 
@@ -48,6 +50,14 @@ public class ProgramView extends JFrame {
 		setContentPane(contentPane);
 		SpringLayout sl_contentPane = new SpringLayout();
 		contentPane.setLayout(sl_contentPane);
+		
+		addWindowListener(new WindowAdapter() {
+	      public void windowClosing(WindowEvent e) {
+	        while(tabbedPane.getTabCount() > 0) {
+	        	CloseChat();
+	        }
+	      }
+	    });
 		
 		tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		sl_contentPane.putConstraint(SpringLayout.NORTH, tabbedPane, 0, SpringLayout.NORTH, contentPane);
